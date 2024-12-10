7d455b26b935ae05dc4a2f6cfd62f338ab794070
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2024-12-10 20:47:48 +0100

    feat: add shadcn

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
-import { defineConfig } from 'vitest/config';
-import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from "vitest/config";
+import { sveltekit } from "@sveltejs/kit/vite";
 
 export default defineConfig({
 	plugins: [sveltekit()],
-
+	server: {
+		proxy: {
+			"/api": {
+				target: "http://localhost:8080",
+				changeOrigin: true,
+				secure: false,
+			},
+		},
+	},
 	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
-	}
+		include: ["src/**/*.{test,spec}.{js,ts}"],
+	},
 });
